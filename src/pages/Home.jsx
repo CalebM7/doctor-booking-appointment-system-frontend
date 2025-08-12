be26466fa11b2139a8d77e838eae5b6d3b4d173a
Caleb Muturi <calebmuturi2024@gmail.com>
2025-08-12 12:36:46 +0300

    Add TopDoctors component to Home page and update Tailwind config for grid layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from '../components/Header'
-import SpecialityMenu from '../components/SpecialityMenu'
+import React from 'react';
+import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
